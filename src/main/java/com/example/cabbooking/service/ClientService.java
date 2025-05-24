029e0edee3e95146629a94b51d7d5dac74eeed67
MegaXmas <noahmendelsohn96@gmail.com>
2025-05-24 16:41:06 -0700

    ClientService created

--- src/main/java/com/example/cabbooking/service/ClientService.java
+++ src/main/java/com/example/cabbooking/service/ClientService.java
@@ -1,4 +1,14 @@
 package com.example.cabbooking.service;
 
-public class ClientService {
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.cabbooking.model.Client;
+
+public interface ClientService {
+
+   List<Client> getAllClients();
+   Optional<Client> getClientById(int id);
+   Client addClient(Client client);
 }
