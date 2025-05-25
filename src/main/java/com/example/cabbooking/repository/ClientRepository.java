c40637e14170bf82a426d49456cdea0f0c4ede71
MegaXmas <noahmendelsohn96@gmail.com>
2025-05-24 18:00:29 -0700

    ClientService and ClientRepository edited. added some methods, trying to figure out correct syntax

--- src/main/java/com/example/cabbooking/repository/ClientRepository.java
+++ src/main/java/com/example/cabbooking/repository/ClientRepository.java
@@ -3,16 +3,17 @@ package com.example.cabbooking.repository;
 import com.example.cabbooking.model.Client;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+@Repository
+public class ClientRepository {
 
-public interface ClientRepository {
+    private Map<String, Client> clients = new HashMap<>();
 
+    List<Client> findAll() { return new ArrayList<Client>(clients.values()); }
 
-    List<Client> findAll();
-    Optional<Client> findById(String id);
+    Optional<Client> findById(Integer id) { return Optional.of(clients.get(id)); }
+
     Client save(Client client);
 
 
--- src/main/java/com/example/cabbooking/service/ClientService.java
+++ src/main/java/com/example/cabbooking/service/ClientService.java
@@ -1,14 +1,22 @@
 package com.example.cabbooking.service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import com.example.cabbooking.model.Client;
+import com.example.cabbooking.repository.ClientRepository;
+import org.springframework.stereotype.Service;
 
-public interface ClientService {
+@Service
+public class ClientService {
 
-   List<Client> getAllClients();
+   List<Client> getAllClients() {
+      return ClientRepository.
+   }
+
+   ;
    Optional<Client> getClientById(int id);
    Client addClient(Client client);
+   Client updateClient(int id, Client client);
 }
