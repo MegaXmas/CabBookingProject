a2f962064a26d2d202aed74f2135b7eb1e118110
MegaXmas <noahmendelsohn96@gmail.com>
2025-05-27 15:43:59 -0700

    create Location.java to store lat and long of a location
    updated MilesDistanceCalculator

---
+++ src/main/java/com/example/cabbooking/model/Location.java
@@ -0,0 +1,51 @@
+package com.example.cabbooking.model;
+
+import com.example.cabbooking.service.MilesDistanceCalculator;
+
+
+class Location {
+    private double latitude;
+    private double longitude;
+    private String name;
+
+    // Constructor - this runs when we create a new Location
+    public Location(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
