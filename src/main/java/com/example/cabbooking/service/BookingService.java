06fe18332dd0643e6c95bb49f757dd5314922435
MegaXmas <noahmendelsohn96@gmail.com>
2025-05-27 15:41:45 -0700

    started CalculateFairService and BookingService

--- src/main/java/com/example/cabbooking/service/BookingService.java
+++ src/main/java/com/example/cabbooking/service/BookingService.java
@@ -1,4 +1,22 @@
 package com.example.cabbooking.service;
 
+import com.example.cabbooking.model.Client;
+import com.example.cabbooking.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingService {
+
+    private ClientRepository clientRepository;
+
+    @Autowired
+    public BookingService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void bookCab(Client client, int miles) {
+        System.out.println("Booking cab " + miles + " miles");
+    }
+
 }
--- src/main/java/com/example/cabbooking/service/CalculateFairService.java
+++ src/main/java/com/example/cabbooking/service/CalculateFairService.java
@@ -1,4 +1,18 @@
 package com.example.cabbooking.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculateFairService {
+
+    public int hourlyWage = 20;
+    public int dollarsPerMile = 3;
+    public int initialBookingFee = 3;
+
+    public int calculateMileage(MilesDistanceCalculator milesDistanceCalculator) {
+
+
+    }
+
+
 }
