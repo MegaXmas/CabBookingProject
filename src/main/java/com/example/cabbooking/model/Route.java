5a2a197d23b7f6cc03412f348c40c063a80c98ca
MegaXmas <noahmendelsohn96@gmail.com>
2025-05-31 18:08:54 -0700

    making Route model. thinking it will be better to use for BookingController and others

---
+++ src/main/java/com/example/cabbooking/model/Route.java
@@ -0,0 +1,39 @@
+package com.example.cabbooking.model;
+
+public class Route {
+
+    private Location from;
+    private Location to;
+    private double distance;
+
+    public Route(Location from, Location to, double distance) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    public void setFrom(Location from) {
+        this.from = from;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public void setTo(Location to) {
+        this.to = to;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
+
