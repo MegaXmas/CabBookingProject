f0b0fbafcbe9b3d1e86e0c81eb1d8fda24bd567a
MegaXmas <noahmendelsohn96@gmail.com>
2025-06-02 15:23:16 -0700

    created simple tests for client getters

--- src/test/java/com/example/cabbooking/model/ClientTest.java
+++ src/test/java/com/example/cabbooking/model/ClientTest.java
@@ -12,14 +12,74 @@ class ClientTest {
     void createNewClient() {
 
         Client client = new Client(4, "mike", "mikemike@test.com",
-                "4444444444", "4444 mike street", "0404040404040404L");
+                "4444444444", "4444 mike street", "0404040404040404");
 
         assertNotNull(client);
         System.out.println(client);
 
         //from this test, I found out using the long wrapper for numbers that may start with 0
-        //converts them into decimal literals, changing credit_card from int -> long -> string
+        //converts them into decimal literals.
+        //changing credit_card from int -> long -> string
     }
 
+    @Test
+    void getClientId() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals(4, client.getId());
+        System.out.println(client);
+    }
+
+    @Test
+    void getClientName() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals("mike", client.getName());
+        System.out.println(client.getName());
+    }
+
+    @Test
+    void getClientEmail() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals("mikemike@test.com", client.getEmail());
+        System.out.println(client.getEmail());
+    }
+
+    @Test
+    void getClientPhone() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals("4444444444", client.getPhone());
+        System.out.println(client.getPhone());
+    }
+
+    @Test
+    void getClientAddress() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals("4444 mike street", client.getAddress());
+        System.out.println(client.getAddress());
+    }
+
+    @Test
+    void getClientCredit_card() {
+
+        Client client = new Client(4, "mike", "mikemike@test.com",
+                "4444444444", "4444 mike street", "0404040404040404");
+
+        assertEquals("0404040404040404", client.getCredit_card());
+        System.out.println(client.getCredit_card());
+    }
 
 }
