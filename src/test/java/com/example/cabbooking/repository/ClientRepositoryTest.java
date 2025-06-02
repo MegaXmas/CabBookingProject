9fe06ba16ac96ab061a71f0481e2fd31d1f87932
MegaXmas <noahmendelsohn96@gmail.com>
2025-06-02 13:45:03 -0700

    started junit testing

---
+++ src/test/java/com/example/cabbooking/repository/ClientRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.example.cabbooking.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientRepositoryTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void newClient() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+}
